147fbd5d1fd63f60aedc35695c995b709e99b125
Mikołaj Woźniak <mikolajw@glitchd.network>
2025-08-08 13:27:36 +0200

    Update

--- listeners/src/Main.sol
+++ listeners/src/Main.sol
@@ -9,7 +9,7 @@ contract Triggers is BaseTriggers {
     function triggers() external virtual override {
         UniswapV4SwapListener listener = new UniswapV4SwapListener();
         addTrigger(
-            chainContract(Chains.Base, 0x498581ff718922c3f8e6a244956af099b2652b2b),
+            chainContract(Chains.Base, 0x498581fF718922c3f8e6A244956aF099B2652b2b),
             listener.triggerOnSwapEvent()
         );
     }
