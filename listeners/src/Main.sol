4281add021bbc53f0efa0a1896763c8a47f0dc4e
Mikołaj Woźniak <mikolajw@glitchd.network>
2025-08-14 14:58:14 +0200

    Add dev filtering

--- listeners/src/Main.sol
+++ listeners/src/Main.sol
@@ -5,18 +5,15 @@ import "sim-idx-sol/Simidx.sol";
 import "sim-idx-generated/Generated.sol";
 import "./UniswapV4SwapListener.sol";
 
-// dev 0x788dde59966ccde6869748d9992386d2d4cd9977
-// prod 0x498581fF718922c3f8e6A244956aF099B2652b2b
-
 contract Triggers is BaseTriggers {
     function triggers() external virtual override {
         UniswapV4SwapListener listener = new UniswapV4SwapListener();
         addTrigger(
-            chainContract(Chains.Base, 0x788DDE59966CCDe6869748D9992386d2d4cd9977),
+            chainContract(Chains.Base, 0x498581fF718922c3f8e6A244956aF099B2652b2b),
             listener.triggerOnSwapEvent()
         );
         addTrigger(
-            chainContract(Chains.Base, 0x788DDE59966CCDe6869748D9992386d2d4cd9977),
+            chainContract(Chains.Base, 0x498581fF718922c3f8e6A244956aF099B2652b2b),
             listener.triggerOnInitializeEvent()
         );
     }
